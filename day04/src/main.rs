622903e9fb8a28ed4bb3f0317269e19def177375
Benjamin Boxler <benjaminboxler@gmail.com>
2017-12-08 15:19:08 +0000

    Day04 part 1

---
+++ day04/src/main.rs
@@ -0,0 +1,31 @@
+use std::fs::File;
+use std::io::prelude::*;
+
+fn main() {
+    let filename = "passwords.txt";
+    let mut f = File::open(filename).expect("file not found");
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        .expect("something went wrong reading the file");
+    let mut password_array: Vec<Vec<&str>> = Vec::new();
+    {
+        let passwords: Vec<&str> = contents.trim().split('\n').collect();
+
+        for (_,p) in passwords.iter().enumerate() {
+            let password = p.trim().split(' ').collect();
+            password_array.push(password);
+        }
+    }
+    let mut sum = password_array.len();
+    for (_,p) in password_array.iter().enumerate() {
+        'password_loop: for (j,q) in p.iter().enumerate() {
+            for (k,r) in p.iter().enumerate() {
+                if r == q && j != k{
+                    sum -= 1;
+                    break 'password_loop
+                }
+            }
+        }
+    }
+    println!("{}", sum)
+}
