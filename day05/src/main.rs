031553ecdd9fde3546ba997d689d700c52203f51
Benjamin Boxler <benjaminboxler@gmail.com>
2017-12-08 18:46:31 +0000

    Day 05 p2

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -9,14 +9,33 @@ fn main() {
         .expect("something went wrong reading the file");
     let mut jump_array: Vec<i32> = contents.trim().split('\n').map(|s| s.parse().unwrap()).collect();
     let mut i:i32 = 0;
+    let mut j: i32 = 0;
     let mut sum = 0;
-    loop {
+
+    // Not an elegant solution - Crashes when out of index
+    //
+    // Part one
+    // loop {
+    //     i += jump_array[i as usize];
+    //     jump_array[j as usize] += 1;
+    //     sum += 1;
+    //     j = i;
+    //     println!("{}",sum);        
+    // }
+
+
+    // Part Two
+     loop {
         i += jump_array[i as usize];
-        jump_array[i as usize] += 1;
+        if jump_array[j as usize] >= 3 {
+            jump_array[j as usize] -= 1;
+        } else {
+            jump_array[j as usize] += 1;
+        }
         sum += 1;
-        println!("{}",sum);
-        if i > jump_array.len() as i32 || i < 0 { break }
-        
+        j = i;
+        println!("{}",sum);        
     }
+    
 
 }
