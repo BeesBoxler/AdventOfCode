4d2eb41d70b53d0cf15553ec31722eaf4b62ad25
Benjamin Boxler <benjaminboxler@gmail.com>
2017-12-09 15:11:48 +0000

    Day 06 p2

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,10 +1,12 @@
 fn main() {
     let input = String::from("14	0	15	12	11	11	3	5	1	6	8	4	9	1	8	4");
-    let input = String::from("0	2	7	0");
+    // let input = String::from("0	2	7	0");
     let mut input:Vec<i32> = input.trim().split('\t').map(|s| s.parse().ok().unwrap()).collect();
     println!("{:?}",input);
+    let mut loop_size = 0;
     let mut sum = 0;
     let mut history:Vec<Vec<i32>> = Vec::new();
+    let mut pos = 0;
     'loopy: loop{
         sum += 1;
         let mut max = (0,0);
@@ -33,11 +35,12 @@ fn main() {
         for (j,k) in history.iter().enumerate() {
             if &input[..] == &k[..] {
                 println!("{:?} : {:?}", &input[..], &k[..]);
+                pos = j+1;
                 break 'loopy
             }
         }
         println!("{}: {:?}", sum, input);
         history.push(input.clone());
     }
-    println!("{}", sum)
+    println!("{}", sum - pos)
 }
