950dd0352a0616e15a7fe34931e6bb0d1e917371
Benjamin Boxler <benjaminboxler@gmail.com>
2017-12-11 14:46:26 +0000

    Day07 p1

--- day07/src/main.rs
+++ day07/src/main.rs
@@ -1,14 +1,49 @@
 use std::io::prelude::*;
 use std::fs::File;
 
+#[derive(Debug)]
+struct Tree {
+    name: String,
+    weight: i32,
+    children: Vec<String>
+}
+
 fn main() {
     let mut file = File::open("input.txt").expect("File not found.");
     let mut input = String::new();
     file.read_to_string(&mut input).expect("Whoops something went wrong.");
 
     let input:Vec<&str> = input.trim().split('\n').collect();
-    let input:Vec<Vec<&str>> = input.into_iter().map(|s| s.trim().split("->").map(|s| s.trim()).collect()).collect();
+    let mut trees: Vec<Tree> = Vec::new();
 
-    println!("{:?}", input);
+    for u in input {
+        let buffer: Vec<&str> = u.trim().split(' ').collect();
+        let name: String = String::from(buffer[0]);
+        let weight:i32 = buffer[1].trim_left_matches('(').trim_right_matches(')').parse::<i32>().ok().unwrap();
+        let mut children: Vec<String> = Vec::new();
+        for (i, u) in buffer.iter().enumerate() {
+            if i < 3 { continue }
+            children.push(String::from(u.trim_right_matches(',')));
+        }
+        trees.push(Tree{name,weight,children})
+    }
+
+    for i in &trees {
+        let mut found = false;
+        'find: for j in &trees {
+            for k in &j.children {
+                if k == &i.name {
+                    // println!("{} : {}", k, &i.name);
+                    found = true;
+                    break 'find
+                }
+            }
+        }
+        if !found {
+            println!("{}", i.name)
+        }
+    }
+
+    // println!("{:?}", trees);
     
 }
