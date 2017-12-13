999a04c2269a097c3001ae7213a01383e50f0b95
Benjamin Boxler <benjaminboxler@gmail.com>
2017-12-13 22:14:42 +0000

    Day 7 p2 (Kind of? requires some manual working

--- day07/src/main.rs
+++ day07/src/main.rs
@@ -12,6 +12,7 @@ struct Tree {
 fn main() {
     let mut file = File::open("input.txt").expect("File not found.");
     let mut input = String::new();
+    let mut root:String = String::new();
     file.read_to_string(&mut input).expect("Whoops something went wrong.");
 
     let input:Vec<&str> = input.trim().split('\n').collect();
@@ -25,9 +26,20 @@ fn main() {
         for (i, u) in buffer.iter().enumerate() {
             if i < 3 { continue }
             children.push(String::from(u.trim_right_matches(',')));
-        }
-        trees.insert(name.clone(), Tree{name,weight,children});
+        };
+        trees.insert(name.clone(), 
+                     Tree{
+                         name,
+                         weight,
+                         children
+                        }
+                    );
+                
     }
+
+    // for (i,v) in &trees {
+    //     println!("{}: {}",i,get_weights(&trees,i));
+    // }
 
     for (i,_) in &trees {
         let mut found = false;
@@ -40,7 +52,49 @@ fn main() {
             }
         }
         if !found {
+            root = i.clone();
             println!("{}", i)
         }
     }    
+
+    match_weights(&trees, &root);
+
+}
+
+fn match_weights(trees:&HashMap<String,Tree>, name: &str) {
+    let tree = &trees[name];
+    // println!("{:?}",tree);
+    let weights:Vec<i32>= tree.children.iter().map(|c| get_weights(trees, c)).collect();
+    for w in &weights {
+        if w != &weights[0] {
+            println!("Weight mismatch: {}: {}", name, w);
+            break
+        }
+    }
+    if tree.children.len() > 0 {
+        println!(
+            "{} ({}) -> {:?}",
+            name,
+            tree.weight,
+            tree.children.iter().zip(weights).collect::<Vec<_>>()
+        );
+    } else {
+        println!("{} ({})", name, tree.weight);
+    }
+    for c in &tree.children {
+        match_weights(&trees, c);
+    }
+}
+
+fn get_weights(trees:&HashMap<String,Tree>, name: &str) -> i32{
+    let mut weight = 0;
+
+    let tree = &trees[name];
+    weight += tree.weight;
+
+    for child in &tree.children {
+        weight += get_weights(trees, child);
+    }
+
+    weight
 }
