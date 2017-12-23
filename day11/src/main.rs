2087c35462c0a539d349c866333793598f1091dd
Benjamin Boxler <benjaminboxler@gmail.com>
2017-12-23 03:40:58 +0000

    Day 10 a

---
+++ day11/src/main.rs
@@ -0,0 +1,67 @@
+use std::io::prelude::*;
+use std::fs::File;
+
+#[derive(Debug)]
+struct Hero {
+    x: i32,
+    y: i32,
+    z: i32
+}
+
+fn main() {
+
+    let mut file = File::open("input.txt").expect("woah there");
+    let mut input = String::new();
+    file.read_to_string(&mut input).expect("That boy ain't right"); //https://youtu.be/y1C8C7op9LU
+
+    // let input = String::from("se,sw,se,sw,sw");
+
+    let mut our_hero = Hero{x:0, y:0, z:0};
+    let zero_hero = Hero{x:0, y:0, z:0};
+
+    let input:Vec<&str> = input.split(",").collect();
+
+    for i in input {
+        match i {
+            "n" => {
+                our_hero.x += 0;
+                our_hero.y += 1;
+                our_hero.z -= 1;
+            },
+            "s" => {
+                our_hero.x += 0;
+                our_hero.y -= 1;
+                our_hero.z += 1;
+            },
+            "ne" => {
+                our_hero.x += 1;
+                our_hero.y += 0;
+                our_hero.z -= 1;
+            },
+            "sw" => {
+                our_hero.x -= 1;
+                our_hero.y += 0;
+                our_hero.z += 1;
+            },
+            "nw" => {
+                our_hero.x -= 1;
+                our_hero.y += 1;
+                our_hero.z += 0;
+            },
+            "se" => {
+                our_hero.x += 1;
+                our_hero.y -= 1;
+                our_hero.z -= 0;
+            },
+
+            _ => {}
+        }
+    };
+
+    println!("Distance from start: {}", hex_distance(zero_hero,our_hero));
+
+}
+
+fn hex_distance(a:Hero, b:Hero) -> i32 {
+    ((a.x - b.x).abs() + (a.y - b.y).abs() + (a.z - b.z).abs()) / 2
+}
