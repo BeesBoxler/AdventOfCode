ddcd65bd0de7c1a5ba48ea82119b49bc87a5ab5b
Benjamin Boxler <benjaminboxler@gmail.com>
2017-12-23 15:02:37 +0000

    fuck this

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,44 +1,75 @@
+
+
+/* 
+ *
+ *   ___        _     _   _    _      
+ *  | __|  _ __| |__ | |_| |_ (_)___  
+ *  | _| || / _| / / |  _| ' \| (_-<  
+ *  |_|_\_,_\__|_\_\  \__|_||_|_/__/  
+ *  |_ _|  __ _(_)_ _____   _  _ _ __ 
+ *   | |  / _` | \ V / -_) | || | '_ \
+ *  |___| \__, |_|\_/\___|  \_,_| .__/
+ *        |___/                 |_|   
+ *
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+extern crate petgraph;
+
 use std::io::prelude::*;
 use std::fs::File;
-use std::collections::HashMap;
+use petgraph::Graph;
+use petgraph::algo::*;
+use petgraph::dot::{Dot, Config};
+use petgraph::visit::*;
+
 
 fn main() {
     let mut file = File::open("input.txt").expect("whoops");
     let mut input = String::new();
     file.read_to_string(&mut input).expect("uh-oh");
+
+    let mut graph = Graph::<i32,i32, petgraph::Undirected>::new_undirected();
+
     let input:Vec<Vec<&str>> = input.split('\n').map(|x| x.split(' ').collect()).collect();
-    println!("{:?}",input);
-    let mut inputs:HashMap<&str, Vec<&str>> = HashMap::new();
+    let mut inputs:Vec<Vec<i32>> = Vec::new();
     let mut sum = 0;
-    for i in input {
-        let mut buffer:Vec<&str> = Vec::new();
+    for i in &input {
+        let mut buffer:Vec<i32> = Vec::new();
         for (j,v) in i.iter().enumerate() {
             if j < 2 {
                 continue
             }
-            buffer.push(v.trim_right_matches(','));
+            buffer.push(v.trim_right_matches(',').parse().ok().unwrap());
         }
-        inputs.insert(i[0],buffer);
-        println!("{}: {:?}",i[0],inputs[i[0]])
+        inputs.insert(i[0].parse().ok().unwrap(),buffer.clone());
     };
-    for (i,v) in &inputs {
-        // if i == &v[0] {
-        //     sum += 1;
-        // }
-        if walk_tree(&inputs, i, i) {
-            sum += 1;
+
+    let mut vec = Vec::new();
+
+
+    for (i,_) in inputs.iter().enumerate() {
+        let i = i as i32;
+        vec.push(graph.add_node(i));
+    }
+    for (i,v) in inputs.iter().enumerate() {
+        for j in v {
+            let j = j.clone() as usize;
+            graph.add_edge(vec[i],vec[j],0);
         }
     }
-    println!("Sum: {}", sum)
-}
-
-fn walk_tree(tree:&HashMap<&str,Vec<&str>>, name:&str, root:&str)->bool{
-    for c in &tree[name] {
-        if c == &"0" { return true }
-        if c == &name || c == &root { return false }
-        if walk_tree(&tree, c, &name) { return true };
-        println!("{}",c);
-        return false
-    }
-    false
+    // println!("{:?}",graph)
+    println!("{:?}", Dot::with_config(&graph, &[Config::EdgeNoLabel]));
+    println!("{}", sum)
 }
